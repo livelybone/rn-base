6a1f9095a5f8cf9c0605dfe3dc96067adbc06785
livelybone <liuzhiping@auraxy.com>
2020-08-01 08:51:54 +0800

    feat: update resolver of metro

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,15 @@
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @format
  */
 
 module.exports = {
+  resolver: {
+    resolverMainFields: ['module', 'browser', 'main'],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
